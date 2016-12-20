8d2a553abc49aab6eff09ea562be93e274f2beca
binking <jiangzhibin2014.xujie@gmail.com>
2016-12-20 12:19:43 +0800

    zhihu topic tree

--- traverse_topic_tree.py
+++ traverse_topic_tree.py
@@ -62,7 +62,9 @@ def traverse_tree_recusively(url, depth=0, max_depth=3):
         ret_dict['is_leaf'] = 'N'
         return ret_dict
     
-    for child in child_list[1:]:
+    for child in child_list:
+        if '未归类'.decode('utf8') in child[0][1]:
+            continue
         # print '%s(%s) -> %s(%s) -> %d' % (msg_list[1], msg_list[2], child[0][1], child[0][2], depth+1)
         if child[0][1] == '加载更多'.decode('utf8'):
             print '加载更多 ...'
